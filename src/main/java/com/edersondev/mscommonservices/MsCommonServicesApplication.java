4b6d91313226e41015193303bf777fd3eaaee1f4
Ederson Ferreira <ederson.dev@gmail.com>
2022-01-21 16:25:44 -0300

    create project

---
+++ src/main/java/com/edersondev/mscommonservices/MsCommonServicesApplication.java
@@ -0,0 +1,13 @@
+package com.edersondev.mscommonservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCommonServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCommonServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edersondev/mscommonservices/MsCommonServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edersondev.mscommonservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCommonServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
