b63011b82c3431d1c8cb71d22ffddef7f712bd70
Ederson Ferreira <ederson.dev@gmail.com>
2022-02-03 14:36:28 -0300

    added restriction for dto data

--- src/main/java/com/edersondev/mscommonservices/dto/person/PersonCreateDTO.java
+++ src/main/java/com/edersondev/mscommonservices/dto/person/PersonCreateDTO.java
@@ -3,12 +3,28 @@ package com.edersondev.mscommonservices.dto.person;
 import java.io.Serializable;
 import java.time.LocalDate;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+
+import com.sun.istack.NotNull;
+
 public class PersonCreateDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@NotBlank
+	@Size(min = 3, max = 150)
 	private String name;
+	
+	@NotNull
+	@DateTimeFormat(iso = ISO.DATE)
+	@PastOrPresent
 	private LocalDate birthday;
+	
 	private Integer gender;
 	
 	public PersonCreateDTO() {}
