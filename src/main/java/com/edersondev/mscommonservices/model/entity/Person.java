2b1de42c839f33d980a5586be49a0083f4a3fefc
Ederson Ferreira <ederson.dev@gmail.com>
2022-02-02 22:36:44 -0300

    class Person and enum Gender

---
+++ src/main/java/com/edersondev/mscommonservices/model/entity/Person.java
@@ -0,0 +1,73 @@
+package com.edersondev.mscommonservices.model.entity;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.edersondev.mscommonservices.model.enums.Gender;
+
+@Entity
+@Table(name = "tb_person")
+public class Person {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String name;
+	
+	@Column(nullable = false)
+	private LocalDate birthday;
+	
+	private Gender gender;
+	
+	public Person() {
+		
+	}
+
+	public Person(Long id, String name, LocalDate birthday, Gender gender) {
+		this.id = id;
+		this.name = name;
+		this.birthday = birthday;
+		this.gender = gender;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public LocalDate getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(LocalDate birthday) {
+		this.birthday = birthday;
+	}
+
+	public Gender getGender() {
+		return gender;
+	}
+
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+	
+}
