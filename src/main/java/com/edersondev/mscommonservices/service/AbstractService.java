578c48717daa13bedf44e39a7dcc335a46295597
Ederson Ferreira <ederson.dev@gmail.com>
2022-02-05 01:26:38 -0300

    added methods show and delete to AbstractService

--- src/main/java/com/edersondev/mscommonservices/service/AbstractService.java
+++ src/main/java/com/edersondev/mscommonservices/service/AbstractService.java
@@ -1,11 +1,16 @@
 package com.edersondev.mscommonservices.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.edersondev.mscommonservices.service.exception.ResourceNotFoundException;
 
 public abstract class AbstractService<Repository extends JpaRepository<Entity,Long>,Entity> {
 
@@ -17,6 +22,19 @@ public abstract class AbstractService<Repository extends JpaRepository<Entity,Lo
 		Sort sort = (sortDir.equals("asc") ? Sort.by(sortBy).ascending() : Sort.by(sortBy).descending());
 		Pageable pageable = PageRequest.of(pageNo, pageSize, sort);
 		return repository.findAll(pageable);
+	}
+	
+	public Entity show(Long id) {
+		Optional<Entity> entity = repository.findById(id);
+		return entity.orElseThrow(() -> new ResourceNotFoundException());
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException();
+		}
 	}
 	
 }
