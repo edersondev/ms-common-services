369b81d01264f34676d685aaff98b8b5133c2148
Ederson Ferreira <ederson.dev@gmail.com>
2022-02-11 11:02:17 -0300

    created entity Document and enum DocumentType

---
+++ src/main/java/com/edersondev/mscommonservices/model/enums/DocumentType.java
@@ -0,0 +1,24 @@
+package com.edersondev.mscommonservices.model.enums;
+
+public enum DocumentType {
+	CPF(1),RG(2),PASSPORT(3);
+	
+	private int code;
+	
+	private DocumentType(int code) {
+		this.code = code;
+	}
+	
+	public int getCode() {
+		return code;
+	}
+	
+	public static DocumentType valueOf(int code) {
+		for(DocumentType value : DocumentType.values()) {
+			if(value.getCode() == code) {
+				return value;
+			}
+		}
+		throw new IllegalArgumentException("Invalid document type");
+	}
+}
