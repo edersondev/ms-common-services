4d5d197c5f9002b8af5ae55c36e6d0a326cf5aa1
Ederson Ferreira <ederson.dev@gmail.com>
2022-03-06 00:18:34 -0300

    updated class GenderConverter

--- src/main/java/com/edersondev/mscommonservices/model/enums/converter/GenderConverter.java
+++ src/main/java/com/edersondev/mscommonservices/model/enums/converter/GenderConverter.java
@@ -1,28 +1,23 @@
 package com.edersondev.mscommonservices.model.enums.converter;
 
-import java.util.stream.Stream;
-
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
 
 import com.edersondev.mscommonservices.model.enums.Gender;
 
 @Converter(autoApply = true)
-public class GenderConverter implements AttributeConverter<Gender, String> {
+public class GenderConverter implements AttributeConverter<Gender, Integer> {
 
 	@Override
-	public String convertToDatabaseColumn(Gender gender) {
+	public Integer convertToDatabaseColumn(Gender gender) {
 		if(gender == null) { return null; }
 		return gender.getCode();
 	}
 
 	@Override
-	public Gender convertToEntityAttribute(String code) {
+	public Gender convertToEntityAttribute(Integer code) {
 		if(code == null) { return null; }
-		return Stream.of(Gender.values())
-				.filter(g -> g.getCode().equals(code))
-				.findFirst()
-				.orElse(null);
+		return Gender.of(code);
 	}
 
 }
